f2219e192cafe704f0a73fe4eba4dc63ae2f08be
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-15 14:04:23 +0530

    main:13th commit

--- src/components/ShowHelthData/ShowHelthData.tsx
+++ src/components/ShowHelthData/ShowHelthData.tsx
@@ -1,9 +1,29 @@
 import { useContext } from "react";
 import mainContext from "../../Store/mainContext/mainContext";
+import HealthTable from "../../common/HealthTable/HealthTable";
 
 const ShowHelthData = () => {
   const healthContext: any = useContext(mainContext);
   console.log("healthContext", healthContext);
-  return <div>ShowHealthData.tsx</div>;
+  const {healthData} = healthContext;
+
+  const tHead = [
+    "id",
+    "name",
+    "DBP",
+    "HR",
+    "SBP",
+    "cal",
+    "hemoglobin",
+    "spo2",
+    "sugar",
+
+  ];
+
+console.log('healthData', healthData);
+
+  return <div>
+    <HealthTable tHead = {tHead} healthData = {healthData} /> 
+  </div>;
 };
 export default ShowHelthData;
