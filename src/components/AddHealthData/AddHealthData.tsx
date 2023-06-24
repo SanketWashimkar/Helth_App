d905d0ec24858fd10fb711cf80f174a4f9bf56ea
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-24 15:02:25 +0530

    main: 28 commit

--- src/components/AddHealthData/AddHealthData.tsx
+++ src/components/AddHealthData/AddHealthData.tsx
@@ -27,7 +27,18 @@ const AddHealthData = () => {
     inputTouched: hrIsTouched,
     setInputTouched: setHrIsTouched,
     inputIsEmpty: hrIsEmpty,
-  }: any = useFormValidation((userInput: any) => userInput === "");
+  }: any = useFormValidation((hrInput: any) => hrInput <= 0);
+
+  const{ input: sbpInput,
+    setInput: setSbpInput,
+    inputTouched: sbpIsTouched,
+    setInputTouched: setSbpIsTouched,
+    inputIsEmpty: sbpIsEmpty }:any = useFormValidation((SbpInput: any) => SbpInput <= 0  );
+
+const isFromValid = userIsEmpty || hrIsEmpty || sbpIsEmpty;
+console.log('userIsEmpty && hrIsEmpty && sbpIsEmpty', userIsEmpty , hrIsEmpty , sbpIsEmpty)
+console.log("isFromValid", isFromValid);
+
 
   const addNewHealthData = (e: any) => {
     e.preventDefault();
@@ -36,9 +47,9 @@ const AddHealthData = () => {
     const newHealthData = [
       ...healthData,
       {
-        id: 12,
-        HR: 12,
-        SBP: 22,
+        id: Math.random(),
+        HR: hrInput,
+        SBP: sbpInput,
         DBP: 32,
         sugar: 42,
         cal: 52,
@@ -59,7 +70,8 @@ const AddHealthData = () => {
   //     setUserName(e.target.value);
   //   };
   return (
-    <div>
+    <div className={styless['container']}>
+      <span className={styless['container-head']}> Add New User Data </span>
       <form onSubmit={(e) => addNewHealthData(e)}>
         <div className={styless["form-control"]}>
           <label htmlFor="username">Username</label>
@@ -101,11 +113,36 @@ const AddHealthData = () => {
           )}
           </div>
         </div>
+        <div className="sm:col-span-2 sm:col-start-1 w-96">
+          <label
+            htmlFor="hr"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Systolic heart rate 
+          </label>
+          <div className="mt-2">
+            <input
+              type="number"
+              name="SBP"
+              id="SBP"
+              value={sbpInput}
+              onBlur={() => setSbpIsTouched(true)}
+              onChange={(e) => setSbpInput(parseFloat(e.target.value))}
+              autoComplete="address-level2"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+             {sbpIsTouched && sbpIsEmpty && (
+            <span className={styless["error-class"]}>
+              SBP can not be empty
+            </span>
+          )}
+          </div>
+        </div>
         <button
           type="submit"
-          disabled={hrIsEmpty}
+          disabled={isFromValid}
           className={
-            hrIsEmpty ? styless["submit-bt-empty"] : styless["submit-bt"]
+            isFromValid ? styless["submit-bt-empty"] : styless["submit-bt"]
           }
         >
           Add New
