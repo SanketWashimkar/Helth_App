ad373e9cf95985a3d937d9a2220b7b7e8a07acb2
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-26 11:59:12 +0530

    main:28 commit

--- src/MenuBar/MenuBar.tsx
+++ src/MenuBar/MenuBar.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import logo from "../logo/React.png";
+import { NavLink } from "react-router-dom";
 
 const MenuBar: React.FC<{
   buttonArr: any;
 }> = ({ buttonArr }) => {
-  const buttonContent = buttonArr.map((bt: any) => {
-    return (
-      <button key={bt.id} className="text-white" onClick={bt.btFun}>
-        {bt.label}
-      </button>
-    );
+  // const buttonContent = buttonArr.map((bt: any) => {
+  //   return (
+  //     <button key={bt.id} className="text-white" onClick={bt.btFun}>
+  //       {bt.label}
+  //     </button>
+  //   );
+  // });
+const buttonContent = buttonArr.map((bt: any) => {
+  return (
+
+    <NavLink className={({isActive})=>isActive ? 'text-green-500' : 'text-white-500'} to = {bt.path}>{bt.label} </NavLink>
+  );
+
   });
   return (
     <div className="flex items-center justify-between bg-blue-500 p-4">
