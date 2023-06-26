2b6ccf60984f8937dd1f4db25b2798db881b9b0c
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-26 11:58:26 +0530

    main:24commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,71 @@ import AddHealthData from "./components/AddHealthData/AddHealthData";
 import AnData from "./components/AnData/AnData";
 import ShowHelthData from "./components/ShowHelthData/ShowHelthData";
 import { MainContexWrapper } from "./Store/mainContext/mainContext";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainComponent from "./MainComponent/MainCompenent";
 
 function App() {
   const [loadComponent, setLoadComponent] = useState(<AddHealthData />);
-  const buttonArr = [
+//  const buttonArr = [
+//    {
+//      id: 1,
+//      label: "Add-Helth",
+//      btFun: () => {
+//         setLoadComponent(<AddHealthData />);
+//      },
+//    },
+//    {
+//      id: 2,
+//      label: "Show-Helth",
+//      btFun: () => {
+//        setLoadComponent(<ShowHelthData />);
+//      },
+//    },
+//    {
+//       id: 3,
+//      label: "Analitics-Helth",
+//      btFun: () => {
+//         setLoadComponent(<AnData />);
+//      },
+//     },
+//  ];
+
+  const router = createBrowserRouter([
+ {
+    path: '/',
+  element: <MainComponent />,
+  children: [
+ 
     {
-      id: 1,
-      label: "Add-Helth",
-      btFun: () => {
-        setLoadComponent(<AddHealthData />);
-      },
+      path: "/",
+      element: <AddHealthData />,
     },
     {
-      id: 2,
-      label: "Show-Helth",
-      btFun: () => {
-        setLoadComponent(<ShowHelthData />);
-      },
+      path: "/showData",
+      element: <ShowHelthData/>,
     },
     {
-      id: 3,
-      label: "Analitics-Helth",
-      btFun: () => {
-        setLoadComponent(<AnData />);
-      },
+      path: "/andata",
+      element: <AnData/>
     },
-  ];
+  ]
+}
+]);
 
   return (
     <MainContexWrapper>
-     
-        <MenuBar buttonArr={buttonArr} />
-        <div className="main-content">
-        {loadComponent}
-      </div>
-    </MainContexWrapper>
-  );
-}
+      
+    {/* 
+    <div className='main-content'>
+  {loadComponent}
+  </div> */}
 
+
+  <div className='main-content'>
+  <RouterProvider router={router} />
+  </div>
+
+</MainContexWrapper>
+);
+}
 export default App;
